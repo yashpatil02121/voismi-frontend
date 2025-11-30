1fbfd03e5dd5e2ea891e43dbe20585a742d9e9eb
YASH SANJAY PATIL <patilyash02121@gmail.com>
2025-11-30 19:52:01 +0530

    Updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center py-20">
-      <h1 className="text-4xl font-bold">Voismi</h1>
+      <h1 className="text-4xl font-bold">Voismi Home</h1>
       <p className="text-gray-600 mt-2">
         International calling for organizations and users
       </p>
 
-      <a
-        href="/auth/login"
-        className="mt-6 px-6 py-2 bg-blue-600 text-white rounded"
-      >
-        Login
-      </a>
+      
     </div>
   );
 }
